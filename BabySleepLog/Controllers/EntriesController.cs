d12f038e171429d6007084db9a88687baac0aeb6
nellip <co.pfeiffer@gmail.com>
2017-04-01 22:46:19 +0200

    added Database from CodeFirst added EntriesController

---
+++ BabySleepLog/Controllers/EntriesController.cs
@@ -0,0 +1,128 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using BabySleepLog.Models;
+
+namespace BabySleepLog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private DataModel db = new DataModel();
+
+        // GET: Entries
+        public ActionResult Index()
+        {
+            var entries = db.Entries.Include(e => e.Sleep);
+            return View(entries.ToList());
+        }
+
+        // GET: Entries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        public ActionResult Create()
+        {
+            ViewBag.SleepId = new SelectList(db.Sleeps, "Id", "Name");
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Date,SleepId,Duration,Level,Exclude,Notes")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entries.Add(entry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SleepId = new SelectList(db.Sleeps, "Id", "Name", entry.SleepId);
+            return View(entry);
+        }
+
+        // GET: Entries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SleepId = new SelectList(db.Sleeps, "Id", "Name", entry.SleepId);
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Date,SleepId,Duration,Level,Exclude,Notes")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SleepId = new SelectList(db.Sleeps, "Id", "Name", entry.SleepId);
+            return View(entry);
+        }
+
+        // GET: Entries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Entry entry = db.Entries.Find(id);
+            db.Entries.Remove(entry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BabySleepLog/DataModel.cs
@@ -0,0 +1,26 @@
+namespace BabySleepLog
+{
+    using Models;
+    using System.Data.Entity;
+
+    public class DataModel : DbContext
+    {
+        // Your context has been configured to use a 'DataModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'BabySleepLog.DataModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'DataModel' 
+        // connection string in the application configuration file.
+        public DataModel()
+            : base("name=DataModel")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+         public virtual DbSet<Entry> Entries { get; set; }
+        public virtual DbSet<Sleep> Sleeps { get; set; }
+    }
+        
+}
