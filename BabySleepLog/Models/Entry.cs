8e524400899819cbc6607b622b82dadd3f9dbe88
nellip <co.pfeiffer@gmail.com>
2017-04-01 22:32:40 +0200

    added datepicker updated _Layout.cshtml added Entry.cs and Sleep.cs

---
+++ BabySleepLog/Models/Entry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BabySleepLog.Models
+{
+    public class Entry
+    {
+        public enum SleepLevel
+        {
+            Shallow,
+            Heavy
+        }
+     
+        public Entry()
+        { }        
+        
+        public int Id { get; set; }
+        
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Sleep")]
+        public int SleepId { get; set; }
+
+        public Sleep Sleep { get; set; }
+       
+        public double Duration { get; set; }
+        
+        public SleepLevel Level { get; set; }
+       
+        public bool Exclude { get; set; }
+
+        [MaxLength(200, ErrorMessage = "The Notes field cannot be longer than 200 characters.")]
+        public string Notes { get; set; }
+    }
+}
---
+++ BabySleepLog/Models/Sleep.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabySleepLog.Models
+{
+    public class Sleep
+    {        
+        public enum SleepType
+        {
+            Night = 1,
+            Morning = 2,
+            Lunch = 3,
+            Afternoon = 4,
+            Irregular = 5
+        }
+      
+        public Sleep(SleepType sleepType, string name = null)
+        {
+            Id = (int)sleepType;            
+            Name = name ?? sleepType.ToString();
+        }
+               
+        public int Id { get; set; }       
+        public string Name { get; set; }
+    }
+}
